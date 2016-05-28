c8154403afd0dc83de60a6d7ef6a1b8185e2fba3
anymuse <anymuse@gmail.com>
2016-05-29 01:43:05 +0800

    initial commit

---
+++ AMSPullToRefresh/Classes/AMSPullToRefresh.h
@@ -0,0 +1,13 @@
+//
+//  AMSPullToRefresh.h
+//  Pods
+//
+//  Created by 朱琨 on 16/5/29.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AMSPullToRefresh : UIView
+
+@end
---
+++ AMSPullToRefresh/Classes/AMSPullToRefresh.m
@@ -0,0 +1,21 @@
+//
+//  AMSPullToRefresh.m
+//  Pods
+//
+//  Created by 朱琨 on 16/5/29.
+//
+//
+
+#import "AMSPullToRefresh.h"
+
+@implementation AMSPullToRefresh
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
